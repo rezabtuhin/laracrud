f29e6d1106dbe071fe9e5676c9660abf7c96ef3f
rezabuddawla <rezabuddawlatuhin@gmail.com>
2023-06-19 17:55:15 +0600

    database update

--- database/migrations/2023_06_19_044537_create_products_table.php
+++ database/migrations/2023_06_19_044537_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('slug');
             $table->string('description')->nullable();
-            $table->decimal('price', 5, 2);
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
