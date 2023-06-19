7a0ff37a40dd4da93748d47e90c7a86098c6d2a3
rezabuddawla <rezabuddawlatuhin@gmail.com>
2023-06-19 13:35:35 +0600

    relationship between table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -16,4 +17,9 @@ class Product extends Model
         'description',
         'price'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
